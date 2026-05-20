79ac04e037240705a05d9feff5538c137c785c0e
Ikwuka Okoye <ikwukao@proton.me>
2026-05-20 13:49:44 -0400

    Add another class

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Create an Email class with the following attributes: 
+# Create an Email class with the following attributes:
 # sender, receiver, subject, body, and has_been_read.
 class Email:
     def __init__(self, sender, receiver, subject, body):
@@ -7,7 +7,11 @@ class Email:
         self.subject = subject
         self.body = body
         self.has_been_read = False
-        
+
     def mark_as_read(self):
         self.has_been_read = True
 
+
+class User:
+    def __init__(self, name):
+        self.name = name
